cbd635d8e68d9a03111ee9b31f11f2193b6b05df
Tommy Nguyen <94974692+seobtr@users.noreply.github.com>
2022-11-10 15:34:26 -0600

    Commit from TeleportHQ

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import DangerousHTML from 'dangerous-html/react'
 import PropTypes from 'prop-types'
 
 const Footer = (props) => {
@@ -86,18 +85,6 @@ const Footer = (props) => {
                 </div>
               </a>
             </div>
-          </div>
-          <div>
-            <DangerousHTML
-              html={`<div class="ec-cart-widget"></div>
-<div>
-    <script data-cfasync="false" type="text/javascript"
-        src="https://app.ecwid.com/script.js?78742521&data_platform=code&data_date=2022-11-10" charset="utf-8"></script>
-    <script type="text/javascript">
-        Ecwid.init();
-    </script>
-</div>`}
-            ></DangerousHTML>
           </div>
         </footer>
       </div>
